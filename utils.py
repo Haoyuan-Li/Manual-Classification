6da9dd4a8a91684d8d75be71142aa82093d55fe2
Haoyuan Li <haoyuan_li_93@outlook.com>
2017-06-16 11:50:41 -0700

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
-def load_data():
-    pass
+import numpy as np
+from tkinter import *
+from tkinter import ttk
+import glob
+
+
+def load_image(ind):
+    pattern = PhotoImage(master=mainframe, file=image_files[0])
+    Img['image'] = pattern
+    
     return
+
+def load_data(address):
+    image_files = glob.glob(address+'*_data.png')
+    load_image(0)
+    return 
 
 def create_label():
     pass
@@ -13,3 +26,43 @@ def load_label():
 def save_label():
     pass
     return
+
+def next_image():
+    pass
+    return
+
+def next_run_number():
+    pass
+    return
+
+def previous_image():
+    pass
+    return
+
+def previous_run_number():
+    pass
+    return
+
+def set_label():
+    pass
+    return
+
+def test():
+    # Use this test function to see whether the data and label are loaded
+    if data == None:
+        print('data is not loaded')
+        return False
+    
+    if label == None:
+        print('label is not loaded')
+        return False
+    
+    return True
+
+def test_data():
+    # Use this test function to see whether the data is loaded
+    if data == None:
+        print('data is not loaded')
+        return False
+    
+    return True
